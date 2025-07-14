fc5798dbc1abea351696386176d7ea6ef304ec97
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-07-14 15:08:52 -0700

    Create create_s3_buckets.py

---
+++ kafka_project/create_s3_buckets.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+from dotenv import load_dotenv
+import toml
+
+load_dotenv()
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+app_config = toml.load('config_file.toml')
+bucket_name = app_config['aws']['bucket_name']
+aws_region = app_config['aws']['aws_region']
+
+session = boto3.Session(
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
+
+s3_client = session.client('s3')
+s3_client.create_bucket(
+    Bucket=bucket_name,  
+    CreateBucketConfiguration={'LocationConstraint': aws_region} 
+)
