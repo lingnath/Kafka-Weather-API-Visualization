ce51469835de86bb9f84dad7d7b186bb3e2e8062
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-07-14 17:41:32 -0700

    Update create_s3_buckets.py

--- kafka_project/create_s3_buckets.py
+++ kafka_project/create_s3_buckets.py
@@ -11,12 +11,14 @@ app_config = toml.load('config_file.toml')
 bucket_name = app_config['aws']['bucket_name']
 aws_region = app_config['aws']['aws_region']
 
+# Connect to S3
 session = boto3.Session(
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY
 )
-
 s3_client = session.client('s3')
+
+# Create S3 bucket in the aws region we specified
 s3_client.create_bucket(
     Bucket=bucket_name,  
     CreateBucketConfiguration={'LocationConstraint': aws_region} 
